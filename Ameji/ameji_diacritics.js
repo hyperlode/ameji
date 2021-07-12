6d5fc85150c256054b1413cc4d8025ef70c48eb2
lode <lodeameije@gmail.com>
2021-07-11 19:05:34 -0700

    add diacritics. Include diacritics for punctuation

--- Ameji/ameji_diacritics.js
+++ Ameji/ameji_diacritics.js
@@ -5,19 +5,23 @@ var ameji_diacritics_JSON = `{
     "sym2":{"id":"sym2","type":"diacritic","files":[["ameji-diacritics","dot-double.png"]],"meaning":["super symbolic"]},
     "verb":{"id":"verb","type":"diacritic","files":[["ameji-diacritics","arrow-right.png"]],"meaning":["verb", "transitive verb"]},
     "verb2":{"id":"verb2","type":"diacritic","files":[["ameji-diacritics","arrow-double.png"]],"meaning":["verb", "intransitive verb"]},
-    "gen":{"id":"gen","type":"diacritic","files":[["ameji-diacritics","squiggle.png"]],"meaning":["valuge", "related to", "has something to do with"]},
+    "gen":{"id":"gen","type":"diacritic","files":[["ameji-diacritics","squiggle.png"]],"meaning":["vaguely relevant", "related to", "has something to do with"]},
     "lit":{"id":"lit","type":"diacritic","files":[["ameji-diacritics","line.png"]],"meaning":["literal meaning"]},
     "blank":{"id":"blank","type":"diacritic","files":[["ameji-diacritics","empty.png"]],"meaning":["no diacritic"]},
     "name":{"id":"name","type":"diacritic","files":[["ameji-diacritics","line-full-width.png"]],"meaning":["proper noun"]},
-    "combo":{"id":"combo","type":"diacritic","files":[["ameji-diacritics","line-dashed-full-width.png"]],"meaning":["combination of symbols"]},
-    "has2":{"id":"has","type":"diacritic","files":[["ameji-diacritics","arrow-compact-down.png"]],"meaning":["has as part"]},
-    "has":{"id":"has2","type":"diacritic","files":[["ameji-diacritics","arrow-down.png"]],"meaning":["has as part"]},
-    "is2":{"id":"is","type":"diacritic","files":[["ameji-diacritics","arrow-compact-up.png"]],"meaning":["is part of"]},
-    "is":{"id":"is2","type":"diacritic","files":[["ameji-diacritics","arrow-up.png"]],"meaning":["is part of"]},
-    "yes":{"id":"yes","type":"diacritic","files":[["ameji-diacritics","checkmark.png"]],"meaning":["yes", "affirmitive", "positive", "right"]},
-    "no":{"id":"no","type":"diacritic","files":[["ameji-diacritics","cross-diagonal.png"]],"meaning":["no", "negative", "wrong"]}
+    "combo":{"id":"combo","type":"diacritic","files":[["ameji-diacritics","line-dashed-full-width.png"]],"meaning":["combines different symbols to form one meaning", "part of a word"]},
+    "has":{"id":"has","type":"diacritic","files":[["ameji-diacritics","arrow-down.png"]],"meaning":["has as part", "has as property", "has in its set of properties"]},
+    "is":{"id":"is","type":"diacritic","files":[["ameji-diacritics","arrow-up.png"]],"meaning":["is part of", "is a property of", "is in the properties set of"]},
+    "sibling":{"id":"sibling","type":"diacritic","files":[["ameji-diacritics","arrow-up-down.png"]],"meaning":["is at same level", "same category", "has same parent", "siblings"]},
+    "sibling2":{"id":"sibling2","type":"diacritic","files":[["ameji-diacritics","arrow-up-noline.png"]],"meaning":["is at same level", "same category", "has same parent", "siblings"]},
+    "no meaning":{"id":"no meaning","type":"diacritic","files":[["ameji-diacritics","line-buckled-down.png"]],"meaning":["NO MEANING"]},
+    "no meaning2":{"id":"no meaning2","type":"diacritic","files":[["ameji-diacritics","line-buckled-up.png"]],"meaning":["NO MEANING"]},
+    "yes":{"id":"yes","type":"diacritic","files":[["ameji-diacritics","checkmark.png"]],"meaning":["yes", "affirmitive", "positive", "right", "relevant"]},
+    "no":{"id":"no","type":"diacritic","files":[["ameji-diacritics","cross-diagonal.png"]],"meaning":["no", "negative", "wrong", "irrelevant", "has nothing to do with"]}
 }`;
 
+// "is2":{"id":"is","type":"diacritic","files":[["ameji-diacritics","arrow-compact-up.png"]],"meaning":["is part of"]},
+// "has2":{"id":"has","type":"diacritic","files":[["ameji-diacritics","arrow-compact-down.png"]],"meaning":["has as part"]},
 
 
 // // "-" = none
